467fc204576dbe9f2c88f298a665e03b11176a1d
bonnyped <bonnyped@student.21-school.ru>
2023-10-27 17:16:27 +0600

    minor changes

---
+++ src/ex00/day03_ex00.sql
@@ -0,0 +1,11 @@
+SELECT pizza_name,
+       price,
+       zz.name pizzeria_name,
+       visit_date
+FROM menu mn
+         RIGHT JOIN person_visits pv ON pv.pizzeria_id = mn.pizzeria_id
+         JOIN person p ON pv.person_id = p.id
+         JOIN pizzeria zz ON zz.id = pv.pizzeria_id
+WHERE p.name IN ('Kate')
+  AND price BETWEEN 800 AND 999
+ORDER BY 1, 2, 3;
---
+++ src/ex01/day03_ex01.sql
@@ -0,0 +1,5 @@
+SELECT mn.id AS menu_id
+FROM menu mn
+WHERE mn.id NOT IN (SELECT po.menu_id
+                    FROM person_order po)
+ORDER BY 1;

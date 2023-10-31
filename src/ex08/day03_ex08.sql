bd522dbe40eacbdefd591f4d71f233705280da00
bonnyped <bonnyped@student.21-school.ru>
2023-11-01 00:02:25 +0600

    minor changes

---
+++ src/ex08/day03_ex08.sql
@@ -0,0 +1,5 @@
+INSERT INTO menu (id, pizzeria_id, pizza_name, price)
+VALUES ((select max(id + 1)
+         from menu), (select zz.id
+                      from pizzeria zz
+                      where name in ('Dominos')), 'sicilian pizza', 900);
